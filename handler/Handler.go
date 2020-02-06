8fefd99790c761af5eea49b33183c2083da29519
Monster <coolpest@gmail.com>
2020-02-06 19:11:49 +0800

    接口设置

--- handler/Handler.go
+++ handler/Handler.go
@@ -3,3 +3,6 @@ package handler
 import "github.com/gin-gonic/gin"
 
 func IndexHandler(c *gin.Context){}
+func GoodFoodHandler(c *gin.Context){}
+func GoodRestrantHandler(c *gin.Context){}
+func MeHandler(c *gin.Context){}
--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/i-coder-robot/mock-food"
+	"github.com/i-coder-robot/mock-food/handler"
 )
 
 func main() {
 	fmt.Println("aaa")
 	r := gin.Default()
 	r.GET("/index",handler.IndexHandler)
+	r.GET("/index",handler.IndexHandler)
+	r.GET("/index",handler.IndexHandler)
+	r.GET("/me",handler.IndexHandler)
 }
