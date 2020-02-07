67c6795f704008fa3aee46308582b5073f718897
Monster <coolpest@gmail.com>
2020-02-07 19:45:32 +0800

    model

--- handler/Handler.go
+++ handler/Handler.go
@@ -1,9 +1,17 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func IndexHandler(c *gin.Context){}
 func GoodFoodHandler(c *gin.Context){}
 func GoodRestaurantHandler(c *gin.Context){}
 func MeHandler(c *gin.Context){}
-func NavHandler(c *gin.Context){}
+func NavHandler(c *gin.Context){
+	items []model.IndexNavItem;
+	c.JSON(http.StatusOK,gin.H{
+
+	})
+}
---
+++ model/indexNavItem.go
@@ -0,0 +1,9 @@
+package model
+
+type IndexNavItem struct {
+	Id string `json:"id"`
+	Src string `json:"src"`
+	LocalPath string `json:"localPath"`
+	Icon string `json:"icon"`
+	Title string `json:"title"`
+}
