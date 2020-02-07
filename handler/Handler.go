db59cfb6d1c8293f27f0046e0430f4709117b3ea
Monster <coolpest@gmail.com>
2020-02-07 19:25:32 +0800

    handler

--- handler/Handler.go
+++ handler/Handler.go
@@ -4,5 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func IndexHandler(c *gin.Context){}
 func GoodFoodHandler(c *gin.Context){}
-func GoodRestrantHandler(c *gin.Context){}
+func GoodRestaurantHandler(c *gin.Context){}
 func MeHandler(c *gin.Context){}
--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/i-coder-robot/mock-food/handler"
 )
 
 func main() {
-	fmt.Println("aaa")
 	r := gin.Default()
 	r.GET("/index",handler.IndexHandler)
-	r.GET("/index",handler.IndexHandler)
-	r.GET("/index",handler.IndexHandler)
-	r.GET("/me",handler.IndexHandler)
+	r.GET("/food",handler.GoodFoodHandler)
+	r.GET("/restaurant",handler.GoodRestaurantHandler)
+	r.GET("/me",handler.MeHandler)
 }
